a2b3eeccfb8ee02f8a614f07c5c27db7cffa9040
stevo32800 <93198283+stevo32800@users.noreply.github.com>
2022-07-06 14:51:21 +0200

    first_commit

---
+++ appAzure.py
@@ -0,0 +1,26 @@
+
+import flask 
+from flask import Flask, app
+import os
+from flask import send_from_directory
+from flask import render_template
+from flask import request
+
+
+app = Flask(__name__)
+@app.route('/')
+def index():
+   print('Request for index page received')
+   return render_template('index.html')
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+@app.route('/hello', methods=['POST'])
+def hello():
+   name = request.form.get('name')
+
+   if name:
+       print('Request for hello page received with name=%s' % name)
